952d822a7698e6881a5d7f97a3dc7bb1f697d7e5
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-11-05 20:16:28 +0000

    RPC implementation in progress.

--- client/client.go
+++ client/client.go
@@ -5,5 +5,6 @@ import (
 )
 
 func main() {
-	fmt.Println("I live!")
+	var name string = "Ryan"
+	fmt.Printf("%s lives!\n", name)
 }
--- server/server.go
+++ server/server.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"grpcdemo/server/rpc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
+type myRpcServer struct {
+}
+
+func (s myRpcServer) GetGreeting(c context.Context, n *rpc.Name) (*rpc.Greeting, error) {
+	return &rpc.Greeting{Greeting: fmt.Sprintf("Hello, %s\n", n.GetName())}, nil
+}
+
 func main() {
-	fmt.Println("I live, again!")
+	listener, err := net.Listen("tcp", ":42069")
+
+	if err != nil {
+		log.Fatalf("Unable to create listener: %s", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &myRpcServer{}
+	rpc.RegisterRpcServer(serverRegistrar, service)
 }
