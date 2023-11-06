1954f0921a13f71998375b6cbca81724c5fa8705
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-11-06 17:49:59 +0000

    Server implementation complete.

--- server/server.go
+++ server/server.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"grpcdemo/greetingrpc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type myGreetingRpcServer struct {
+	greetingrpc.UnimplementedGreetingRpcServer
+}
+
+func (m myGreetingRpcServer) GetGreeting(c context.Context, n *greetingrpc.Name) (*greetingrpc.Greeting, error) {
+	return &greetingrpc.Greeting{Greeting: fmt.Sprintf("Hello, %s\n", n.String())}, nil
+}
 
 func main() {
-	fmt.Println("I live, again!")
+	const netw string = "tcp"
+	const addr string = ":42069"
+	lis, err := net.Listen(netw, addr)
+
+	fmt.Printf("server --> (%s) %s\n", netw, addr)
+
+	if err != nil {
+		log.Fatalf("unable to create listener: %s\n", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &myGreetingRpcServer{}
+
+	greetingrpc.RegisterGreetingRpcServer(serverRegistrar, service)
+	if serverRegistrar.Serve(lis) != nil {
+		log.Fatalf("unable to serve: %s\n", err)
+	}
 }
